d33be9e623fada31285d1cbaafa2ab235e6a462b
Chris Hamblin <chrishamblin7@gmail.com>
2021-11-12 21:46:44 -0500

    small

--- submit_force_prune.py
+++ submit_force_prune.py
@@ -3,7 +3,7 @@ from subprocess import call
 config = 'configs/alexnet_sparse_config.py'
 layers = ['features_3','features_6','features_8','features_10']
 data_path = 'image_data/imagenet_2/'
-units = [19]
+units = range(19)
 Ts = [1,8]
 ratios = [.5,.2,.1,.05,.01,.005,.001]
 device = 'cuda:0'
