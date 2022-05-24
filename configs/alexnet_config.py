e67a0af6b0f3106f72d2a9dc221f8189af74c52b
Chris Hamblin <chrishamblin7@gmail.com>
2022-05-24 14:18:03 -0400

    edited configs

--- configs/alexnet_config.py
+++ configs/alexnet_config.py
@@ -22,10 +22,6 @@ model = models.alexnet(pretrained=True)
 
 ###DATA PATH###
 
-if not os.path.exists(root_path+'/image_data/imagenet_2'):
-	from circuit_pruner.download_from_gdrive import download_from_gdrive
-	download_from_gdrive('alexnet_sparse',target = 'images')
-
 data_path =  root_path+'/image_data/imagenet_2'   #Set this to the system path for the folder containing input images you would like to see network activation maps for.
 
 label_file_path = root_path+'/image_data/imagenet_labels.txt'      #line seperated file with names of label classes as they appear in image names
